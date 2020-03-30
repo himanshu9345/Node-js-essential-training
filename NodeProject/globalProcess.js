67c86b6fdc6b6368c08e3f58fd9152c2b89e7825
himanshu9345 <hphimanshu6@gmail.com>
2020-03-30 16:27:17 -0400

    learning process object

--- NodeProject/globalProcess.js
+++ NodeProject/globalProcess.js
@@ -1,2 +1,15 @@
 console.log(process.pid);
 console.log(process.versions.node);
+console.log(process.argv[2]);
+console.log('your name is '+process.argv[2]);
+
+const grab = flag => {
+    let indexAfterFlag=process.argv.indexOf(flag)+1
+    return process.argv[indexAfterFlag]
+}
+
+const  name=grab("--name");
+console.log(name);
+
+const  lastname=grab("--lastname");
+console.log(lastname);
